997ace67a0c047e9c3b10fbfd8184d02cbf99c08
Jim Veneskey <github@strifecrafter.com>
2017-02-17 12:42:21 -0500

    Finished some more UI boilerplate code

---
+++ DreamLister/DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by jim Veneskey on 2/17/17.
+//  Copyright © 2017 Strifecrafter. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+}
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 Left off
 
-https://www.udemy.com/devslopes-ios10/learn/v4/t/lecture/5619418
+https://www.udemy.com/devslopes-ios10/learn/v4/t/lecture/5619458
+
 
 
 
